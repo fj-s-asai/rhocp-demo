fcd7297071573b741d703159e0cd723d46fdd817
yuu <fj7267hc@aa.jp.fujitsu.com>
2020-08-04 18:43:04 +0900

    dd

--- setup.sh
+++ setup.sh
@@ -15,28 +15,34 @@ oc apply -f ./frontweb/BuildConfig.yml
 oc apply -f ./frontweb/Service.yml
 oc apply -f ./frontweb/DestinationRule.yml
 oc apply -f ./frontweb/VirtualService.yml
-#oc apply -f ./frontweb/Deployment.yml
-oc apply -f ./frontweb/DeploymentConfig.yml
+oc apply -f ./frontweb/Deployment.yml
+#oc apply -f ./frontweb/DeploymentConfig.yml
 
 
 # backweb1  ------------------------------------------
 oc apply -f ./backweb1/ImageStream.yml
 oc apply -f ./backweb1/BuildConfig.yml
+oc apply -f ./backweb1/BuildConfig_v11.yml
 oc apply -f ./backweb1/Service.yml
 oc apply -f ./backweb1/DestinationRule.yml
 oc apply -f ./backweb1/VirtualService.yml
-#oc apply -f ./backweb1/Deployment.yml
-oc apply -f ./backweb1/DeploymentConfig.yml
+oc apply -f ./backweb1/Deployment.yml
+oc apply -f ./backweb1/Deployment_v11.yml
+#oc apply -f ./backweb1/DeploymentConfig.yml
+#oc apply -f ./backweb1/DeploymentConfig_v11.yml
 
 
-# backweb1 ------------------------------------------
+
+
+
+# backweb2 ------------------------------------------
 oc apply -f ./backweb2/ImageStream.yml
 oc apply -f ./backweb2/BuildConfig.yml
 oc apply -f ./backweb2/Service.yml
 oc apply -f ./backweb2/DestinationRule.yml
 oc apply -f ./backweb2/VirtualService.yml
-#oc apply -f ./backweb2/Deployment.yml
-oc apply -f ./backweb2/DeploymentConfig.yml
+oc apply -f ./backweb2/Deployment.yml
+#oc apply -f ./backweb2/DeploymentConfig.yml
 
 
 
@@ -55,8 +61,8 @@ oc apply -f ./restdb/BuildConfig.yml
 oc apply -f ./restdb/Service.yml
 oc apply -f ./restdb/DestinationRule.yml
 oc apply -f ./restdb/VirtualService.yml
-#oc apply -f ./restdb/Deployment.yml
-oc apply -f ./restdb/DeploymentConfig.yml
+oc apply -f ./restdb/Deployment.yml
+#oc apply -f ./restdb/DeploymentConfig.yml
 
 
 oc project cds
