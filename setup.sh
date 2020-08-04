b7697604753db9750d1cde4c7247ba90e3a93d24
yuu <fj7267hc@aa.jp.fujitsu.com>
2020-08-04 18:09:18 +0900

    dd

--- setup.sh
+++ setup.sh
@@ -12,28 +12,31 @@ oc apply -f ./istio/VirtualService.yml
 # frontweb  ------------------------------------------
 oc apply -f ./frontweb/ImageStream.yml
 oc apply -f ./frontweb/BuildConfig.yml
-oc apply -f ./frontweb/Deployment.yml
-#oc apply -f ./frontweb/DeploymentConfig.yml
 oc apply -f ./frontweb/Service.yml
 oc apply -f ./frontweb/DestinationRule.yml
+oc apply -f ./frontweb/VirtualService.yml
+#oc apply -f ./frontweb/Deployment.yml
+oc apply -f ./frontweb/DeploymentConfig.yml
 
 
 # backweb1  ------------------------------------------
 oc apply -f ./backweb1/ImageStream.yml
 oc apply -f ./backweb1/BuildConfig.yml
-oc apply -f ./backweb1/Deployment.yml
-#oc apply -f ./backweb1/DeploymentConfig.yml
 oc apply -f ./backweb1/Service.yml
 oc apply -f ./backweb1/DestinationRule.yml
+oc apply -f ./backweb1/VirtualService.yml
+#oc apply -f ./backweb1/Deployment.yml
+oc apply -f ./backweb1/DeploymentConfig.yml
 
 
 # backweb1 ------------------------------------------
 oc apply -f ./backweb2/ImageStream.yml
 oc apply -f ./backweb2/BuildConfig.yml
-oc apply -f ./backweb2/Deployment.yml
-#oc apply -f ./backweb2/DeploymentConfig.yml
 oc apply -f ./backweb2/Service.yml
 oc apply -f ./backweb2/DestinationRule.yml
+oc apply -f ./backweb2/VirtualService.yml
+#oc apply -f ./backweb2/Deployment.yml
+oc apply -f ./backweb2/DeploymentConfig.yml
 
 
 
@@ -41,18 +44,19 @@ oc apply -f ./backweb2/DestinationRule.yml
 oc apply -f ./mysql/ImageStream.yml
 oc apply -f ./mysql/pvc.yml
 oc apply -f ./mysql/BuildConfig.yml
-oc apply -f ./mysql/DeploymentConfig.yml
 oc apply -f ./mysql/Service.yml
+oc apply -f ./mysql/DeploymentConfig.yml
 
 
 
 # restdb  ------------------------------------------
 oc apply -f ./restdb/ImageStream.yml
 oc apply -f ./restdb/BuildConfig.yml
-oc apply -f ./restdb/Deployment.yml
-#oc apply -f ./restdb/DeploymentConfig.yml
 oc apply -f ./restdb/Service.yml
 oc apply -f ./restdb/DestinationRule.yml
+oc apply -f ./restdb/VirtualService.yml
+#oc apply -f ./restdb/Deployment.yml
+oc apply -f ./restdb/DeploymentConfig.yml
 
 
 oc project cds
