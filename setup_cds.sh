3c9ddf9a963a75ffe62a53ea336da0b0f5d23571
yuu <fj7267hc@aa.jp.fujitsu.com>
2020-08-06 19:12:46 +0900

    dd

---
+++ setup_cds.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+
+#
+# cds project ******************************************
+#
+oc project cds
+
+
+#
+# build phase ==========================================
+#
+# cdsupdater  ------------------------------------------
+oc apply -f ./cdsupdater/ImageStream.yml
+oc apply -f ./cdsupdater/pvc.yml
+oc apply -f ./cdsupdater/BuildConfig.yml
+
+# cds  ------------------------------------------
+oc apply -f ./cds/ImageStream.yml
+oc apply -f ./cds/BuildConfig.yml
+oc apply -f ./cds/Service.yml
+oc apply -f ./cds/Route.yml
+
+
+sleep 120
+
+
+#
+# Deploy Phase =========================================
+#
+
+# cdsupdater  ------------------------------------------
+oc apply -f ./cdsupdater/Job.yml
+
+sleep 30
+
+# cdsupdater  ------------------------------------------
+oc apply -f ./cds/DeploymentConfig.yml.yml
+
+
+
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
